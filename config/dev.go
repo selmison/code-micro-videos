b281c9f3fabd9c620ae4d28b0fe54c0440189e92
Selmison Campelo de Miranda <selmison@mpdft.mp.br>
2020-08-10 15:28:26 -0300

    feature: include 'dev' environment

--- config/dev.go
+++ config/dev.go
@@ -1,10 +1,11 @@
-// +build integration
+// +build dev
 
 package config
 
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/selmison/code-micro-videos/models"
@@ -14,6 +15,7 @@ const (
 	AddressServer = "127.0.0.1:3333"
 	DBDrive       = "postgres"
 	DBName        = "code-micro-videos"
+	DBHost        = "localhost"
 	DBPort        = 5432
 	DBUser        = "postgres"
 	DBPass        = "postgres"
@@ -21,7 +23,15 @@ const (
 )
 
 var (
-	DBConnStr string
+	DBConnStr = fmt.Sprintf(
+		"host=%s port=%d dbname=%s user=%s password=%s sslmode=%s",
+		DBHost,
+		DBPort,
+		DBName,
+		DBUser,
+		DBPass,
+		DBSSLMode,
+	)
 )
 
 func ClearCategoriesTable(dbDriver, dbConnStr string) error {
