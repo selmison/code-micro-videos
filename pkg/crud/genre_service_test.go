fb3558ed657f861231c3744a2ea52196a73bd370
Selmison Campelo de Miranda <selmison@mpdft.mp.br>
2020-08-12 17:02:17 -0300

    test: include tests to add, remove and update in 'genre_service_test'

--- pkg/crud/genre_service_test.go
+++ pkg/crud/genre_service_test.go
@@ -4,16 +4,242 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/golang/mock/gomock"
 
 	"github.com/selmison/code-micro-videos/models"
 	"github.com/selmison/code-micro-videos/pkg/crud"
 	"github.com/selmison/code-micro-videos/pkg/crud/mock"
 	"github.com/selmison/code-micro-videos/pkg/logger"
+	"github.com/selmison/code-micro-videos/pkg/storage/sqlboiler"
 )
+
+func TestAddGenre(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockR := mock.NewMockRepository(ctrl)
+	fakeName := strings.ToLower(faker.FirstName())
+	type fields struct {
+		r sqlboiler.Repository
+	}
+	type returns struct {
+		c models.Genre
+		e error
+	}
+	type args struct {
+		dto crud.GenreDTO
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    returns
+		wantErr bool
+	}{
+		{
+			name:    "When GenreDTO is not provided",
+			args:    args{crud.GenreDTO{}},
+			want:    returns{models.Genre{}, logger.ErrIsRequired},
+			wantErr: true,
+		},
+		{
+			name: "When the name in GenreDTO is blank",
+			args: args{crud.GenreDTO{
+				Name: "    ",
+			}},
+			want:    returns{models.Genre{}, logger.ErrIsRequired},
+			wantErr: true,
+		},
+		{
+			name: "When name in GenreDTO already exists",
+			args: args{crud.GenreDTO{
+				Name: strings.ToLower(faker.FirstName()),
+			}},
+			want:    returns{models.Genre{}, logger.ErrAlreadyExists},
+			wantErr: true,
+		},
+		{
+			name: "When GenreDTO is right",
+			args: args{crud.GenreDTO{
+				Name: fakeName,
+			}},
+			want: returns{models.Genre{
+				Name: fakeName,
+			}, nil},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.want.e != logger.ErrIsRequired {
+				mockR.EXPECT().
+					AddGenre(tt.args.dto).
+					Return(tt.want.e)
+			}
+			s := crud.NewService(mockR)
+			err := s.AddGenre(tt.args.dto)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddGenre() error = '%v', wantErr '%v'", err, tt.wantErr)
+				return
+			}
+			if !errors.Is(err, tt.want.e) {
+				t.Errorf("AddGenre() got = '%v', want '%v'", err, tt.want.e)
+			}
+		})
+	}
+}
+
+func Test_service_RemoveGenre(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockR := mock.NewMockRepository(ctrl)
+	indexRandom := rand.Intn(len(seedsArray))
+	fakeNames := [2]string{
+		faker.FirstName(),
+		seedsArray[indexRandom].Name,
+	}
+	type fields struct {
+		r sqlboiler.Repository
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    error
+		wantErr bool
+	}{
+		{
+			name:    "When name is blank",
+			args:    args{"     "},
+			want:    fmt.Errorf("'name' %w", logger.ErrIsRequired),
+			wantErr: true,
+		},
+		{
+			name:    "When name is not found",
+			args:    args{fakeNames[0]},
+			want:    fmt.Errorf("%s: %w", fakeNames[0], logger.ErrNotFound),
+			wantErr: true,
+		},
+		{
+			name:    "When name is found",
+			args:    args{fakeNames[1]},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "When name is not found" {
+				mockR.EXPECT().
+					RemoveGenre(tt.args.name).
+					Return(tt.want)
+			} else if tt.name == "When name is found" {
+				mockR.EXPECT().
+					RemoveGenre(tt.args.name).
+					Return(tt.want)
+			}
+			s := crud.NewService(mockR)
+			err := s.RemoveGenre(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RemoveGenre() error: %v, wantErr: %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(err, tt.want) {
+				t.Errorf("RemoveGenre() got: '%v', want: '%v'", err, tt.want)
+			}
+		})
+	}
+}
+
+func Test_service_UpdateGenre(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockR := mock.NewMockRepository(ctrl)
+	const (
+		fakeExistName        = "fakeExistName"
+		fakeDoesNotExistName = "fakeDoesNotExistName"
+	)
+	type fields struct {
+		r sqlboiler.Repository
+	}
+	type args struct {
+		name string
+		dto  crud.GenreDTO
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    error
+		wantErr bool
+	}{
+		{
+			name: "When name is blank",
+			args: args{
+				"     ",
+				crud.GenreDTO{
+					Name: faker.FirstName(),
+				},
+			},
+			want:    logger.ErrIsRequired,
+			wantErr: true,
+		},
+		{
+			name:    "When GenreDTO is not provided",
+			args:    args{fakeExistName, crud.GenreDTO{}},
+			want:    logger.ErrIsRequired,
+			wantErr: true,
+		},
+		{
+			name: "When name is not found",
+			args: args{
+				fakeDoesNotExistName,
+				crud.GenreDTO{
+					Name: faker.FirstName(),
+				},
+			},
+			want:    fmt.Errorf("%s: %w", fakeDoesNotExistName, logger.ErrNotFound),
+			wantErr: true,
+		},
+		{
+			name: "When name is found and GenreDTO is provided",
+			args: args{
+				fakeExistName,
+				crud.GenreDTO{
+					Name: faker.FirstName(),
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "When name is not found" || tt.name == "When name is found and GenreDTO is provided" {
+				mockR.EXPECT().
+					UpdateGenre(tt.args.name, tt.args.dto).
+					Return(tt.want)
+			}
+			s := crud.NewService(mockR)
+			err := s.UpdateGenre(tt.args.name, tt.args.dto)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UpdateGenre() error: %v, wantErr: %v", err, tt.wantErr)
+				return
+			}
+			if !errors.Is(err, tt.want) {
+				t.Errorf("UpdateGenre() got: '%v', want: '%v'", err, tt.want)
+			}
+		})
+	}
+}
 
 func Test_service_GetGenres(t *testing.T) {
 	ctrl := gomock.NewController(t)
