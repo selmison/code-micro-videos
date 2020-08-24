18946ff323548a0f5afe4dc008d3974cc3a3b8d2
Selmison Campelo de Miranda <selmison@mpdft.mp.br>
2020-08-24 14:19:35 -0300

    feature: ensure validated video_dto is right

--- pkg/crud/video_dto.go
+++ pkg/crud/video_dto.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/go-playground/validator/v10/non-standard/validators"
 
+	"github.com/selmison/code-micro-videos/models"
 	"github.com/selmison/code-micro-videos/pkg/logger"
 )
 
@@ -14,7 +15,7 @@ var videoValidate *validator.Validate
 type VideoRating int16
 
 const (
-	FreeRating VideoRating = iota
+	FreeRating VideoRating = iota + 1
 	TenRating
 	TwelveRating
 	FourteenRating
@@ -22,12 +23,12 @@ const (
 	EighteenRating
 )
 
-func (v VideoRating) String() string {
-	return [...]string{"Free", "10", "12", "14", "16", "18"}[v]
+func (v *VideoRating) String() string {
+	return [...]string{"Free", "10", "12", "14", "16", "18"}[*v]
 }
 
-func (v VideoRating) Validate() error {
-	switch v {
+func (v *VideoRating) Validate() error {
+	switch *v {
 	case FreeRating, TenRating, TwelveRating, FourteenRating, SixteenRating, EighteenRating:
 		return nil
 	}
@@ -35,21 +36,54 @@ func (v VideoRating) Validate() error {
 }
 
 type VideoDTO struct {
-	Title        string      `json:"title" validate:"not_blank"`
-	Description  string      `json:"description"`
-	YearLaunched int16       `json:"year_launched"`
-	Opened       bool        `json:"opened"`
-	Rating       VideoRating `json:"rating"`
-	Duration     int16       `json:"duration"`
+	Title        string        `json:"title" validate:"not_blank"`
+	Description  string        `json:"description"`
+	YearLaunched *int16        `json:"year_launched" validate:"required"`
+	Opened       bool          `json:"opened"`
+	Rating       *VideoRating  `json:"rating" validate:"required"`
+	Duration     *int16        `json:"duration" validate:"required"`
+	Categories   []CategoryDTO `json:"categories"`
+	Genres       []GenreDTO    `json:"genres"`
 }
 
-func (c *VideoDTO) Validate() error {
-	err := videoValidate.Struct(c)
+func MapVideoToDTO(video models.Video) (*VideoDTO, error) {
+	categoriesDTO := make([]CategoryDTO, len(video.R.Categories))
+	for i, category := range video.R.Categories {
+		categoriesDTO[i] = CategoryDTO{
+			Name:        category.Name,
+			Description: category.Description.String,
+		}
+	}
+	genresDTO := make([]GenreDTO, len(video.R.Genres))
+	for i, genre := range video.R.Genres {
+		genresDTO[i] = GenreDTO{
+			Name: genre.Name,
+		}
+	}
+	rating := VideoRating(video.Rating)
+	dto := &VideoDTO{
+		Title:        video.Title,
+		Description:  video.Description,
+		YearLaunched: &video.YearLaunched,
+		Opened:       video.Opened.Bool,
+		Rating:       &rating,
+		Duration:     &video.Duration,
+		Categories:   categoriesDTO,
+		Genres:       genresDTO,
+	}
+	if err := dto.Validate(); err != nil {
+		return nil, err
+	}
+	return dto, nil
+}
+
+func (v *VideoDTO) Validate() error {
+	err := videoValidate.Struct(v)
 	if err != nil {
 		vErrs := err.(validator.ValidationErrors)
-		return fmt.Errorf("%s %w", vErrs[0].StructField(), logger.ErrIsRequired)
+		return fmt.Errorf("'%s' field %w", vErrs[0].StructField(), logger.ErrIsRequired)
 	}
-	if err := c.Rating.Validate(); err != nil {
+	if err := v.Rating.Validate(); err != nil {
 		return err
 	}
 	return nil
